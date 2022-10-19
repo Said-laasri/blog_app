4d7146bd0467a4abec3b642d7f3fc4d4fba616e1
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-19 17:09:49 +0100

    Fix all rubocop error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User',foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
   has_many :likes, foreign_key: 'user_id'
 
