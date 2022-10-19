1fe6ef103a1f46b3d5770e626fa310a36ff28c20
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-19 15:26:42 +0100

    Config route and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/blog'
+  # get 'pages/blog'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    root "pages#blog"
+    # root "pages#blog"
   # Defines the root path route ("/")
   # root "articles#index"
 end
