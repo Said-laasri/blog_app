687662b24193a27c8f1b63d0a8c5eee378f5cf5d
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-20 15:49:23 +0100

    Test comments

---
+++ spec/model/comment_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject { Comment.new(post_id: '1', author_id: '2', text: 'Hi Tom!') }
+
+  before { subject.save }
+
+  it 'author id should be present' do
+    subject.author_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'post id should be present' do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'text should be a string ' do
+    subject.text = 123
+    expect(subject).to_not be_valid
+  end
+
+  it 'text should be present' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+end
