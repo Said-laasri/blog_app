8d33afa51a109d96201d04be2603091edf675baf
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-20 15:56:34 +0100

    Update foreign key

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :post, foreign_key: 'user_id'
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   after_save :update_comment_counter
 
