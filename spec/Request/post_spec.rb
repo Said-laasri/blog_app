0e2919138191854e0edd3ddf2c205c438d6bae89
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-25 14:50:35 +0100

    Rubocop error correction

--- spec/Request/post_spec.rb
+++ spec/Request/post_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 # testting post controllers
 
-RSpec.describe 'Posts', type: :request  do
+RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     before(:example) do
       get '/users/7/posts'
     end
 
-      it 'it renders the index template' do
-        expect(response).to render_template(:index)
+    it 'it renders the index template' do
+      expect(response).to render_template(:index)
     end
 
-       it 'should return 200' do
-        expect(response).to have_http_status(:ok)
+    it 'should return 200' do
+      expect(response).to have_http_status(:ok)
     end
 
-      it ' should return place holder' do
-        expect(response.body).to include('Here is a list of posts for a given user')
+    it ' should return place holder' do
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 end
