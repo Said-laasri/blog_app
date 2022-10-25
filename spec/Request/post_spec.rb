1459480a9426250eb38406d2ce8eca9fc6004ff7
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-25 14:45:35 +0100

    Create Post controler spec test

---
+++ spec/Request/post_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+# testting post controllers
+
+RSpec.describe 'Posts', type: :request  do
+  describe 'GET /index' do
+    before(:example) do
+      get '/users/7/posts'
+    end
+
+      it 'it renders the index template' do
+        expect(response).to render_template(:index)
+    end
+
+       it 'should return 200' do
+        expect(response).to have_http_status(:ok)
+    end
+
+      it ' should return place holder' do
+        expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+end
+
+RSpec.describe PostsController, type: :request do
+  describe 'GET show' do
+    before(:example) do
+      get '/users/7/posts/6'
+    end
+
+    it 'it renders the show template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'should return 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it ' should return place holder' do
+      expect(response.body).to include('showing list of posts for a given user')
+    end
+  end
+end
