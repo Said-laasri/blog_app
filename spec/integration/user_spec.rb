54f7f08a7103a2e1cf2fb5d223246d0636722d76
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-31 15:48:06 +0100

    Test name all user and posts counter

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :system do
-    describe 'index page' do
-      it 'i can see the names of all users' do
-        visit "/"
-        expect(page).to has_content?(User.all.name)
-      end
+  # describe 'index page' do
+  #   it 'i can see the names of all users' do
+  #     visit "/"
+  #     expect(page).to have_content(User.first.name)
+  #   end
+  # end
+
+  describe 'index page' do
+    it 'i can see the number of post for each user' do
+      visit "/"
+      expect(page).to have_content(User.posts_counter)
     end
   end
+end
