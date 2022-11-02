c331654280eb589f778713bb0d809798e609c602
Hamma Azarok <hammaagazarok@gmail.com>
2022-11-02 12:24:31 +0200

    implement reset password functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,12 +50,41 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  
 
+  # config.action_mailer.perform_caching = false
+  # config.assets.quiet = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'localhost:3000'
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+
+  # SMTP settings for gmail
+#   config.action_mailer.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => "gmail.com",
+#   :user_name            => "hammaagazarok@gmail.com",
+#   :password             => "iztjaghqhvmdentv",
+#   :authentication       => :login,
+#   :enable_starttls_auto => true
+# }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "hammaagazarok@gmail.com",
+    password: "iztjaghqhvmdentv", 
+    domain: "smtp.gmail.com",
+    openssl_verify_mode: "none",
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -72,8 +101,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
+  
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
